e7f8ec22a0fbcb79283a6e775b09c60e9d10ccde
Atobiloye Usama Adedayo <try2rym@yahoo.com>
2021-09-14 15:48:24 +0100

    Worked on animations

--- Function.js
+++ Function.js
@@ -1,20 +1,23 @@
-ScrollReveal().reveal("html", { delay: 1500, opacity: 0 });
-
-// ScrollReveal().reveal(".dog_image", {
-//   delay: 2500,
-//   opacity: 0
-// });
-ScrollReveal().reveal("h1", {
-  delay: 3000,
+ScrollReveal().reveal(".explore_btn", { delay: 1500, opacity: 0 });
+ScrollReveal().reveal("h1, h2, h3", {
+  delay: 1000,
   origin: "left",
   distance: "300px",
   duration: 600,
   opacity: 0
 });
-ScrollReveal().reveal("p, .dog_image", {
-  delay: 3500,
+ScrollReveal().reveal("p, .dog_image, .mld_img", {
+  delay: 1500,
   origin: "bottom",
   distance: "300px",
   duration: 600,
+  opacity: 0
+});
+ScrollReveal().reveal(".sl, .gr, .ptwg", {
+  delay: 1000,
+  origin: "bottom",
+  distance: "70px",
+  interval: 300,
+  duration: 300,
   opacity: 0
 });
